1b6e28cd749dabaa45157b47b016c28beba79113
Hussain <hussainms704@gmail.com>
2026-08-13 17:01:10 +0530

    Expose app instance in wsgi.py for Vercel deployment

--- mealmate/mealmate/wsgi.py
+++ mealmate/mealmate/wsgi.py
@@ -8,10 +8,9 @@ https://docs.djangoproject.com/en/6.1/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mealmate.settings')
 
 application = get_wsgi_application()
-app = application
+app = application  # <--- Essential for Vercel
